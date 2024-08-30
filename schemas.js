271018f72f3ee2f89fb65aa4351129e139103120
FedericoDeniard <fededeniard@gmail.com>
2024-08-30 13:25:11 -0300

    Schema and model

---
+++ schemas.js
@@ -0,0 +1,10 @@
+import { mongoose } from "./myApp";
+
+const Schema = mongoose.Schema;
+const personSchema = new Schema({
+  name: { type: String, required: true },
+  age: Number,
+  favoriteFoods: [String],
+});
+
+const Person = mongoose.model("Person", personSchema);
